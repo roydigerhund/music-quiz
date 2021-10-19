eb484b25c7ac6959420d82f7d5657f96ce3f82d5
Matthias Martin <martin@zoom7.de>
2021-10-20 00:51:17 +0200

    size

--- src/components/OptionItem.tsx
+++ src/components/OptionItem.tsx
@@ -14,7 +14,7 @@ const OptionItem = ({ option, position, isCloned }: Props) => {
       className={classNames(
         'w-full h-full bg-white text-indigo-600 font-bold text-center leading-tight rounded-xl flex items-center justify-center select-none overflow-hidden',
         !option.iconPath &&
-          (option.name.length > 3 ? 'text-sm xs:text-base lg:text-2xl' : 'text-xl xs:text-2xl lg:text-3xl'),
+          (option.name.length > 4 ? 'text-sm xs:text-base lg:text-2xl' : 'text-xl xs:text-2xl lg:text-3xl'),
         position === OptionPosition.DRAGGING && 'z-10 shadow-xl',
         position === OptionPosition.DRAGGING ? 'cursor-[grabbing]' : 'cursor-[grab]',
         isCloned && 'hidden',
